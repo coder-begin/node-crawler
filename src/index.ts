bd75f391fa856a5f0ea4d9b1864f134736ccfb20
shiyu <shiyu@cjzc.net.cn>
2018-09-26 17:35:58 +0800

    框架

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Engine from './lib/Engine';
+import Spierfrom from './lib/Spider';
+import Scheduler from './lib/Scheduler';
+import Downloader from './lib/Downloader';
+
+
+let crawler = (targetUrl:String)=>{
+
+     let engin:Engine = new Engine();
+     
+
+
+
+};
+
+
+crawler('ssss');
+export default crawler;
+
+
---
+++ src/lib/Downloader.ts
@@ -0,0 +1,6 @@
+export default class Downloader {
+    constructor(){
+
+    }
+}
+
